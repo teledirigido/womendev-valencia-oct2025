c0ff9c8f5dada484bcb07ac0d6178aeb1b6e5b6d
Miguel Garrido <hi@miguel.nz>
2025-11-01 02:33:21 +0700

    course updated

--- app/components/SiteLogos.vue
+++ app/components/SiteLogos.vue
@@ -1,4 +1,5 @@
 <template>
+  <hr>
   <div class="course-logos">
     <img src="/images/course/Cofinanciado_por_la_UE_1.webp" loading="lazy">
     <img src="/images/course/EOI_Ministerio_de_Industria_y_Turismo_1.webp" loading="lazy">
@@ -8,9 +9,15 @@
 </template>
 
 <style scoped>
+hr {
+  margin-top: 2rem;
+  border:0 none;
+  height:1px;
+  background-color: #CCC;
+}
 .course-logos {
   max-width: 70%;
-  margin:5rem auto 0;
+  margin:2rem auto 0;
   display: flex;
   width: 100%;
   gap:3rem;
